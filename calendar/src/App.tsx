b12bad2f9602fa42f27d6128ab7283469b588b7b
Jules Caoeiros <jules.caoeiros@gmail.com>
2024-11-30 19:35:51 +0100

    made navbar and added routes for home, calendar and statistics

--- calendar/src/App.tsx
+++ calendar/src/App.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Calendar from "./components/Calendar";
+import Statistics from "./components/Statistics";
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
 
 const App = () => {
-  return (
-    <main>
-      <Routes>
-        <Route path="/" element={<Calendar />} />
-      </Routes>
-    </main>
-  )
-}
 
-export default App
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content */}
+      <main className="flex-1 p-10 overflow-y-auto ml-64">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
