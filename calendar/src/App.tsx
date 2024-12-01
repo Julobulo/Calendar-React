0f3b9a245444fed94df541123a42de82d1c9ac71
Jules Caoeiros <jules.caoeiros@gmail.com>
2024-12-01 18:55:10 +0100

    added Login page if no token are present under Home

--- calendar/src/App.tsx
+++ calendar/src/App.tsx
@@ -3,6 +3,8 @@ import Calendar from "./components/Calendar";
 import Statistics from "./components/Statistics";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Cookies from "js-cookie";
 
 const App = () => {
 
@@ -14,7 +16,12 @@ const App = () => {
       {/* Main Content */}
       <main className="flex-1 p-10 overflow-y-auto ml-64">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <div className="bg-gray-100 p-10">
+              <Home />
+              {Cookies.get('token') && <Login />}
+            </div>
+          } />
           <Route path="/calendar" element={<Calendar />} />
           <Route path="/statistics" element={<Statistics />} />
         </Routes>
