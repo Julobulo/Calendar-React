c8c077f7deb2e54fc7043b9aa26d672f5a691abb
Jules Caoeiros <jules.caoeiros@gmail.com>
2024-12-02 12:23:46 +0100

    added events rendering, with colors

--- calendar/src/components/Calendar.tsx
+++ calendar/src/components/Calendar.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import { UserActivity } from "../../../backend/models/UserActivityModel";
 
 const Calendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -97,6 +98,217 @@ const Calendar = () => {
 
   const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  const renderDayCell = (day: number, month: number, year: number, activities: UserActivity[]) => {
+    // Find activities for the given day
+    const dateString = new Date(year, month, day).toISOString().split("T")[0];
+    const activityForDay = activities.find(
+      (activity: UserActivity) => new Date(activity.date).toISOString().split("T")[0] === dateString
+    );
+
+    return (
+      <div
+        key={day}
+      >
+
+        {/* Display activities */}
+        {activityForDay && (
+          <div className="mt-2 flex flex-col items-center space-y-1">
+            {activityForDay.entries.map((entry: UserActivity["entries"][0], index: number) => (
+              <div
+                key={index}
+                style={{
+                  "backgroundColor": colors[entry.activity] || "#ffffff", // Default color if no match found
+                }}
+                className={`text-xs text-gray-600 text-black rounded px-2 py-1`}
+              >
+                {entry.duration} - {entry.description}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const activities = [
+    {
+      "_id": "64e9a79b59e0f77a33c8ab01",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-01T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Piano Practice",
+          "duration": "45min",
+          "description": "Practiced Moonlight Sonata."
+        },
+        {
+          "activity": "Jogging",
+          "duration": "30min",
+          "description": "Jogged around the park."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab02",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-02T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Reading",
+          "duration": "1h",
+          "description": "Finished 'Atomic Habits'."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab03",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-04T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Cooking",
+          "duration": "1h 30min",
+          "description": "Tried a new pasta recipe."
+        },
+        {
+          "activity": "Workout",
+          "duration": "45min",
+          "description": "Weight training at the gym."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab04",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-07T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Cycling",
+          "duration": "2h",
+          "description": "Rode through the countryside."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab05",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-09T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Gardening",
+          "duration": "1h",
+          "description": "Planted some winter vegetables."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab06",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-10T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Yoga",
+          "duration": "30min",
+          "description": "Morning yoga session."
+        },
+        {
+          "activity": "Coding",
+          "duration": "3h",
+          "description": "Worked on the calendar project."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab07",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-12T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Swimming",
+          "duration": "1h",
+          "description": "Swam at the local pool."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab08",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-15T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Hiking",
+          "duration": "4h",
+          "description": "Explored the forest trails."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab09",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-18T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Photography",
+          "duration": "2h",
+          "description": "Took landscape photos by the river."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab10",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-20T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Writing",
+          "duration": "1h 15min",
+          "description": "Worked on my personal blog."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab11",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-25T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "Celebrating Christmas",
+          "duration": "All day",
+          "description": "Family gathering and gift exchange."
+        }
+      ]
+    },
+    {
+      "_id": "64e9a79b59e0f77a33c8ab12",
+      "userId": "64e9a79b59e0f77a33c8ab60",
+      "date": "2024-12-31T00:00:00.000Z",
+      "entries": [
+        {
+          "activity": "New Year's Eve Party",
+          "duration": "5h",
+          "description": "Celebrated with friends."
+        }
+      ]
+    }
+  ];
+  const colors = {
+    "Piano Practice": "#FF5733",
+    "Jogging": "#33FF57",
+    "Reading": "#3357FF",
+    "Cooking": "#FFC300",
+    "Workout": "#FF33A1",
+    "Cycling": "#33FFF5",
+    "Gardening": "#A833FF",
+    "Yoga": "#FF8C33",
+    "Coding": "#337BFF",
+    "Swimming": "#33FFB8",
+    "Hiking": "#FF3384",
+    "Photography": "#8C33FF",
+    "Writing": "#B8FF33",
+    "Celebrating Christmas": "#FF5733",
+    "New Year's Eve Party": "#FF33FF"
+  };
+
   return (
     <div className="grid grid-rows-[auto,1fr] h-full">
       {/* Header */}
@@ -152,15 +364,19 @@ const Calendar = () => {
           return (
             <div
               key={i}
-              className="p-4 border rounded-lg hover:bg-gray-200 cursor-pointer flex  justify-center"
+              className="p-4 border rounded-lg hover:bg-gray-200 cursor-pointer flex justify-center"
               onClick={(e) => handleDayClick(e, day)}
             >
               {isToday ? (
                 <div className="flex h-8 w-8 items-center justify-center rounded-full bg-blue-600 text-white font-bold">
                   {day}
+                  {renderDayCell(day, month, year, activities)}
                 </div>
               ) : (
-                day
+                <div>
+                  {day}
+                  {renderDayCell(day, month, year, activities)}
+                </div>
               )}
             </div>
           );
