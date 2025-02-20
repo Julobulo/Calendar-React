9e900cf39227b6be92306e018ca614f4cb550bc0
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-02-20 19:13:35 +0100

    using useState to store parameters

--- calendar/src/components/Day.tsx
+++ calendar/src/components/Day.tsx
@@ -1,8 +1,21 @@
-const Day = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+const Day = () => {
+    const [year, setYear] = useState('');
+    const [month, setMonth] = useState('');
+    const [day, setDay] = useState('');
+    const params = useParams();
+    useEffect(() => {
+            setYear(params.year);
+            setMonth(params.month);
+            setDay(params.day);
+            console.log(`params: ${JSON.stringify(params)}`);
+        }, [params]);
+    
     return (
         <div>
-            day component
+            day component for year: {year}, month: {month}, day: {day}.
         </div>
     )
 }
