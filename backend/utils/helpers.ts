7730c2e10d2ad57a1379d728c8a562c2843c49ce
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-03-01 22:55:27 +0100

    Create helpers.ts

---
+++ backend/utils/helpers.ts
@@ -0,0 +1,47 @@
+// function that checks if a token is valid, and if so, it returns the "id" field stored in the payload of the token
+export const checkToken = async (token: string, secret: string): Promise<string | boolean> => {
+    const encoder = new TextEncoder();
+
+    const [encodedHeader, encodedPayload, encodedSignature] = token.split(".");
+
+    if (!encodedHeader || !encodedPayload || !encodedSignature) {
+        return false;
+    }
+
+    const signedData = `${encodedHeader}.${encodedPayload}`;
+
+    const signature = Uint8Array.from(
+        atob(encodedSignature.replace(/-/g, "+").replace(/_/g, "/")),
+        (c) => c.charCodeAt(0),
+    );
+
+    const key = await crypto.subtle.importKey(
+        // Import secret key
+        "raw",
+        encoder.encode(secret),
+        { name: "HMAC", hash: "SHA-256" },
+        false,
+        ["verify"],
+    );
+
+    const isValid = await crypto.subtle.verify(
+        // verify the signature of the token
+        "HMAC",
+        key,
+        signature,
+        encoder.encode(signedData),
+    );
+
+    if (!isValid) {
+        return false;
+    }
+
+    const payload = JSON.parse(
+        atob(encodedPayload.replace(/-/g, "+").replace(/_/g, "/")),
+    ); // Decode the payload
+    if (payload.exp && Date.now() >= payload.exp * 1000) {
+        return false; // token expired
+    }
+
+    return payload.id; // return id that was in payload
+}
