fa6be980162ca0646629ac2c7ecd0aa86219048e
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-03-29 15:16:43 +0100

    Create Statistics.ts

---
+++ backend/routes/Statistics.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import * as Realm from "realm-web";
+
+// The Worker's environment bindings
+type Bindings = {
+    ATLAS_APPID: string;
+    ATLAS_APIKEY: string;
+    JWT_SECRET: string; // private key used to sign jwt tokens
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    REDIRECT_URI: string;
+};
+
+const StatisticsRoute = new Hono<{ Bindings: Bindings }>();
+
+let App: Realm.App;
+
+
+
+
+
+export default StatisticsRoute
