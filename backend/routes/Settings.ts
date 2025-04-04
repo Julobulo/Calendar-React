19fee5432537872bcfffb0fa758c93e26fc7516c
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-04-04 19:38:21 +0200

    created import route

--- backend/routes/Settings.ts
+++ backend/routes/Settings.ts
@@ -21,7 +21,6 @@ let App: Realm.App;
 
 
 SettingsRoute.get('/export', async (c) => {
-    const { year, month, day } = c.req.queries();
     App = App || new Realm.App(c.env.ATLAS_APPID);
     const credentials = Realm.Credentials.apiKey(c.env.ATLAS_APIKEY);
     const user = await App.logIn(credentials);
@@ -51,9 +50,67 @@ SettingsRoute.get('/export', async (c) => {
     }
 
     const data = await activityCollection
-        .find({ userId: new ObjectId(id.toString()) }, { sort: { date: 1 }});
+        .find({ userId: new ObjectId(id.toString()) }, { sort: { date: 1 } });
 
     return c.json(data);
+})
+
+SettingsRoute.post('/import', async (c) => {
+    App = App || new Realm.App(c.env.ATLAS_APPID);
+    const credentials = Realm.Credentials.apiKey(c.env.ATLAS_APIKEY);
+    const user = await App.logIn(credentials);
+    const client = user.mongoClient("mongodb-atlas");
+    const activityCollection = client
+        .db("calendar")
+        .collection<UserActivity>("activity");
+
+    const cookieHeader = c.req.header("Cookie");
+    if (!cookieHeader) {
+        c.status(400);
+        return c.json({ message: "no cookie found" });
+    }
+
+    const cookies = cookieHeader.split(";").map((cookie) => cookie.trim());
+    let token = cookies.find((cookie) => cookie.startsWith(`token=`));
+    if (!token) {
+        c.status(400);
+        return c.json({ message: "no token found" });
+    }
+    token = token.split("=")[1].trim();
+
+    const id = await checkToken(token, c.env.JWT_SECRET);
+    if (!id) {
+        c.status(400);
+        return c.json({ message: "bad token" });
+    }
+
+    const body = await c.req.json();
+
+    // Prepare all entries
+    const entries = body.map((entry: any) => ({
+        userId: new ObjectId(id.toString()),
+        date: new Date(entry.date),
+        entries: entry.entries || [],
+        note: entry.note || "",
+        variables: entry.variables || [],
+    }));
+
+    const results = [];
+    for (const entry of entries) {
+        const existingActivity = await activityCollection.findOne({
+            userId: new ObjectId(id.toString()),
+            date: entry.date,
+        });
+        console.log(`existing activity: ${JSON.stringify(existingActivity)}`);
+
+        if (!existingActivity) {
+            console.log(`activity doesn't exist`);
+            // Insert if the day does not exist
+            const insertResult = await activityCollection.insertOne(entry);
+            results.push(insertResult);
+        }
+    }
+    return c.json({ message: `${results.length} days imported successfully` });
 })
 
 export default SettingsRoute
