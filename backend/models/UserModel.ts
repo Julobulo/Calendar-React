29c9b22ea09f86e3c298d00797ffd79bda28671b
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-04-19 16:18:47 +0200

    added savedLocations field to UserModel

--- backend/models/UserModel.ts
+++ backend/models/UserModel.ts
@@ -10,19 +10,25 @@ interface AppleAuth {
     apple_id: string;
 }
 
+interface Location {
+    name: string;
+    lat: number;
+    lng: number;
+}
+
 export interface User {
     _id: ObjectId;
     email: string;
     username: string;
     createdAt: Date;
     authentication: GoogleAuth | AppleAuth;
-    // colors: { [id: string]: string; }; // stores colors in hex for each activity
     colors: {
         activities: { [activity: string]: string };
         note: string;
         variables: { [variable: string]: string };
     };
     names: string[];
+    savedLocations?: Location[]; 
 }
 
 // Utility type for creating new users without an _id field
