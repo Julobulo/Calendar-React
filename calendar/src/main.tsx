ac29fdbf8c84af6cf5671104adc52cfc6fc400e5
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-04-20 00:01:15 +0200

    fixing leaflet icons bugs
    leaflet was trying to get the icons from the current domain by default but instead I'm telling leaflet to get the icons in their domain

--- calendar/src/main.tsx
+++ calendar/src/main.tsx
@@ -3,6 +3,19 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom"
 import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix default icon paths
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
