b6819fee8fd35245defe5500744ec7ea8309d43e
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-04-19 16:30:53 +0200

    added savedLocations route to get all saved locations

--- backend/routes/Location.ts
+++ backend/routes/Location.ts
@@ -1,5 +1,8 @@
 import { Hono } from "hono";
 import * as Realm from "realm-web";
+import { checkToken } from "../utils/helpers";
+import { User } from "../models/UserModel";
+import { ObjectId } from "bson";
 
 // The Worker's environment bindings
 type Bindings = {
@@ -15,6 +18,39 @@ const LocationRoute = new Hono<{ Bindings: Bindings }>();
 
 let App: Realm.App;
 
+LocationRoute.get('/savedLocations', async (c) => {
+    App = App || new Realm.App(c.env.ATLAS_APPID);
+    const credentials = Realm.Credentials.apiKey(c.env.ATLAS_APIKEY);
+    const user = await App.logIn(credentials);
+    const client = user.mongoClient("mongodb-atlas");
+    const userCollection = client
+        .db("calendar")
+        .collection<User>("users")
 
+    const cookieHeader = c.req.header("Cookie");
+    if (!cookieHeader) {
+        c.status(400);
+        return c.json({ message: "no cookie found" });
+    }
+
+    const cookies = cookieHeader.split(";").map((cookie) => cookie.trim());
+    let token = cookies.find((cookie) => cookie.startsWith(`token=`));
+    if (!token) {
+        c.status(400);
+        return c.json({ message: "no token found" });
+    }
+    token = token.split("=")[1].trim();
+
+    const id = await checkToken(token, c.env.JWT_SECRET);
+    if (!id) {
+        c.status(400);
+        return c.json({ message: "bad token" });
+    }
+
+    const currentUser: User | null = await userCollection.findOne({ _id: new ObjectId(id.toString()) });
+    if (!currentUser) { c.status(400); return c.json({ message: "no user found" })}
+
+    return c.json(currentUser.savedLocations || []);
+})
 
 export default LocationRoute
