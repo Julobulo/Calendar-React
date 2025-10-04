f7ec5c88ca61ee88a37adbbd37338b2c5f3d7c78
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-10-04 22:45:35 +0200

    Using a single Env conf for the whole backend

--- backend/src/utils/types.ts
+++ backend/src/utils/types.ts
@@ -13,3 +13,22 @@ export interface AppError {
   status: StatusCode;
   message: string;
 };
+
+export type Env = {
+    ATLAS_APPID: string;
+    ATLAS_APIKEY: string;
+    JWT_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    REDIRECT_URI: string;
+    JWT_ACCESS_SECRET: string;
+    JWT_REFRESH_SECRET: string;
+    ACCESS_TOKEN_TTL: string;
+    REFRESH_TOKEN_TTL: string;
+    COOKIE_DOMAIN: string;
+    NODE_ENV: string;
+    MONGODB_APP_ID: string;
+    MONGODB_API_KEY: string;
+    MONGODB_CLUSTER: string;
+    MONGODB_DATABASE: string;
+};
