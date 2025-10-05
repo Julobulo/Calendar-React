71f1b94f6f121cc8b4951aafaa33c9aff30ee2f8
Jules Caoeiros <jules.caoeiros@gmail.com>
2025-10-05 12:35:32 +0200

    Update auth.ts
    to the new login system with refresh_tokens and a different google oauth

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,16 +1,24 @@
 import type { Context, Next } from "hono";
 import { getCookie } from "hono/cookie";
-import { checkToken } from "../utils/helpers";
+import { jwtVerify } from "jose";
 
 export async function accessGuard(c: Context, next: Next) {
-  const token = getCookie(c, "token");
+  const token = getCookie(c, "access_token");
   if (!token) return c.json({ error: "Unauthorized" }, 401);
 
   try {
-    const id = await checkToken(token, c.env.JWT_SECRET);
-    if (!id) return c.json({ error: "Unauthorized" }, 401);
-    const payload = { id };
-    c.set("user", payload); // for now just id
+    const ACCESS_SECRET = new TextEncoder().encode(c.env.JWT_ACCESS_SECRET!);
+    const { payload } = await jwtVerify(token, ACCESS_SECRET);
+    c.set("user", payload);
+    // c.set("user", {
+    //   id: payload.id,
+    //   email: payload.email,
+    // });
+    console.log(`user payload: ${payload}, or other payload: ${JSON.stringify({
+      id: payload.id,
+      email: payload.email,
+      role: payload.role ?? "user",
+    })}`)
     return next();
   } catch {
     return c.json({ error: "Unauthorized" }, 401);
